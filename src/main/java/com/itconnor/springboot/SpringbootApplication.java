77c8afe63636fcaf520a547f62f7082744af3889
Connor <3176995257@qq.com>
2025-03-12 00:06:05 +0800

    1.2

--- src/main/java/com/itconnor/springboot/SpringbootApplication.java
+++ src/main/java/com/itconnor/springboot/SpringbootApplication.java
@@ -2,7 +2,7 @@ package com.itconnor.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*123*/
+/*123445666*/
 @SpringBootApplication
 public class SpringbootApplication {
 
