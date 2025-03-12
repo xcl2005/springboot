fa9a864d1a1f3ad172bb6320013b09abdb754e0b
Connor <3176995257@qq.com>
2025-03-13 01:42:38 +0800

    1.3

--- src/main/java/com/itconnor/springboot/SpringbootApplication.java
+++ src/main/java/com/itconnor/springboot/SpringbootApplication.java
@@ -1,6 +1,4 @@
 package com.itconnor.springboot;
-
-import com.itconnor.springboot.controller.JsonController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
